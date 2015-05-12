45dc929e342dcea83f11535ea3ffbd4b885253de
Will Mayner <wmayner@gmail.com>
2015-05-12 15:13:29 -0500

    Simplify code a bit

--- pyemd/emd.pyx
+++ pyemd/emd.pyx
@@ -22,30 +22,18 @@ cdef extern from "lib/emd_hat.hpp":
 # NumPy array to C++ vector conversion
 # ====================================
 
-# See http://stackoverflow.com/a/2434208/1085344
-cdef vector[double] _c_array_to_vector(double* array, int length):
-    cdef vector[double] output_vector
-    output_vector.reserve(length)
-    output_vector.assign(array, array + length)
-    return output_vector
-
-
 # See https://github.com/cython/cython/wiki/tutorials-NumpyPointerToC
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def _np_array_to_vector(np.ndarray[double, ndim=1, mode="c"] array):
+cdef vector[double] _np_array_to_vector(
+    np.ndarray[double, ndim=1, mode="c"] array):
     """Convert a 1D numpy array to a C++ vector.
 
     :param array: A 1D numpy array of ``np.double``.
-
     """
     cdef vector[double] output_vector
-    cdef int length
-    length = array.size
-
-    # Pass pointer to beginning of numpy array data
-    output_vector = _c_array_to_vector(&array[0], length)
-
+    output_vector.reserve(array.size)
+    output_vector.assign(&array[0], &array[0] + array.size)
     return output_vector
 
 
@@ -53,15 +41,12 @@ def _2d_np_array_to_2d_vector(np.ndarray[double, ndim=2, mode="c"] matrix):
     """Convert a 2D numpy array to a C++ vector of vectors.
 
     :param array: A 2D numpy array of ``np.double``.
-
     """
     cdef vector[vector[double]] output_matrix
+    output_matrix.resize(array.shape[0])
     cdef vector[double] c_row
-
-    for row in matrix:
-        c_row = _np_array_to_vector(row)
-        output_matrix.push_back(c_row)
-
+    for i in range(matrix.shape[0]):
+        output_matrix[i] = _np_array_to_vector(row)
     return output_matrix
 
 
@@ -80,24 +65,23 @@ def emd(np.ndarray[double, ndim=1, mode="c"] first_signature,
         :math:`N`..
     :param distance_matrix: A 2D numpy array of ``np.double``, of size
         :math:`N \cross N`.
-
     """
-    # Validation
-    if ([arg.dtype for arg in [first_signature, second_signature,
-                               distance_matrix]] != [np.dtype('float64')] * 3):
-        raise ValueError("Signatures and ditance matrix must be of dtype " +
-                         "'float64'.")
-    N = first_signature.shape[0]
-    if (N != second_signature.shape[0]):
-        raise ValueError("Signatures must be the same size.")
-    if ((N != distance_matrix.shape[0]) or (N != distance_matrix.shape[1])):
-        raise ValueError("Distance matrix must be NxN, where N is the length of" +
-                         "the signatures.")
-    if (first_signature.ndim != 1) or (first_signature.ndim !=
-                                       second_signature.ndim):
-        raise ValueError("Signatures must be 1-dimensional.")
-    if (distance_matrix.ndim != 2):
-        raise ValueError("Distance matrix must be 2-dimensional.")
+    # # Validation
+    # if ([arg.dtype for arg in [first_signature, second_signature,
+    #                            distance_matrix]] != [np.dtype('float64')] * 3):
+    #     raise ValueError('Signatures and distance matrix must be of NumPy '
+    #                      'datatype `float64`.')
+    # N = first_signature.shape[0]
+    # if (N != second_signature.shape[0]):
+    #     raise ValueError('Signatures must be the same size.')
+    # if ((N != distance_matrix.shape[0]) or (N != distance_matrix.shape[1])):
+    #     raise ValueError('Distance matrix must be NxN, where N is the length of '
+    #                      'the signatures.')
+    # if (first_signature.ndim != 1) or (first_signature.ndim !=
+    #                                    second_signature.ndim):
+    #     raise ValueError('Signatures must be 1-dimensional.')
+    # if (distance_matrix.ndim != 2):
+    #     raise ValueError('Distance matrix must be 2-dimensional.')
 
     # Convert numpy input to C++ vectors
     cdef vector[double] c_first_signature
