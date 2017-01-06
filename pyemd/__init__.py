b1558aa9ad764dbda7c71e917e0f034b5c23a863
Will Mayner <wmayner@gmail.com>
2017-01-06 12:35:24 -0600

    Update module docstring

--- pyemd/__init__.py
+++ pyemd/__init__.py
@@ -7,11 +7,16 @@ PyEMD
 
 PyEMD is a Python wrapper for `Ofir Pele and Michael Werman's implementation of
 the Earth Mover's Distance
-<http://www.ariel.ac.il/sites/ofirpele/fastemd/code/>`_ that integrates it with
-NumPy.
+<http://www.ariel.ac.il/sites/ofirpele/fastemd/code/>`_ that allows it to be
+used with NumPy.
 
 **If you use this code, please cite the papers listed at the end of the
 README.**
+
+Use PyEMD like so:
+
+Usage
+~~~~~
 
     >>> from pyemd import emd
     >>> import numpy as np
@@ -21,29 +26,34 @@ README.**
     >>> emd(first_signature, second_signature, distance_matrix)
     3.5
 
+You can also get the associated minimum-cost flow:
 
-Limitations and Caveats:
-------------------------
+    >>> from pyemd import emd_with_flow
+    >>> emd_with_flow(first_signature, second_signature, distance_matrix)
+    (3.5, [[0.0, 0.0], [0.0, 1.0]])
+
+
+Limitations and Caveats
+~~~~~~~~~~~~~~~~~~~~~~~
 
 - ``distance_matrix`` must be symmetric.
 - ``distance_matrix`` is assumed to represent a true metric. This must be
-  enforced by the caller. See the documentation in lib/emd_hat.hpp.
+  enforced by the caller. See the documentation in ``pyemd/lib/emd_hat.hpp``.
+- The flow matrix does not contain the flows to/from the extra mass bin.
 - The signatures and distance matrix must be numpy arrays of ``np.float``. The
   original C++ template function can accept any numerical C++ type, but this
   wrapper only instantiates the template with ``double`` (Cython converts
   ``np.float`` to ``double``). If there's demand, I can add support for other
   types.
-- The original C++ functions have optional parameters ``extra_mass_penalty``
-  and ``F`` (for flows); this wrapper does not expose those parameters. See
-  the documentation in lib/emd_hat.hpp.
 
 
-Credits
--------
+Credit
+~~~~~~
 
-- All credit for the actual algorithm and implementation goes to Ofir Pele and
-  Michael Werman. See the `relevant paper
-  <http://www.ariel.ac.il/sites/ofirpele/publications/ICCV2009.pdf>`_.
+- All credit for the actual algorithm and implementation goes to `Ofir Pele
+  <http://www.ariel.ac.il/sites/ofirpele/>`_ and `Michael Werman
+  <http://www.cs.huji.ac.il/~werman/>`_. See the `relevant paper
+  <http://www.seas.upenn.edu/~ofirpele/publications/ICCV2009.pdf>`_.
 - Thanks to the Cython devlopers for making this kind of wrapper relatively
   easy to write.
 
