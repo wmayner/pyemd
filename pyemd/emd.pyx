5f6518e32e9531026c5e6915b39406c2ed333d11
Will Mayner <wmayner@gmail.com>
2017-01-06 12:05:24 -0600

    Use Google docstring format

--- pyemd/emd.pyx
+++ pyemd/emd.pyx
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # distutils: language = c++
+# emd.pyx
 
 from libcpp.vector cimport vector
 from libcpp.pair cimport pair
@@ -36,23 +37,25 @@ def emd_with_flows(np.ndarray[np.float64_t, ndim=1, mode="c"] first_signature,
                    np.ndarray[np.float64_t, ndim=2, mode="c"] distance_matrix,
                    extra_mass_penalty=-1.0):
     """
-    Compute the Earth Mover's Distance between the given signature with the
-    given distance matrix.
-    Return a tuple containing the EMD value and the flows array.
+    Compute the EMD between signatures with the given distance matrix.
 
-    :param first_signature: A 1D numpy array of ``np.double``, of length
-        :math:`N`.
-    :param second_signature: A 1D numpy array of ``np.double``, also of length
-        :math:`N`..
-    :param distance_matrix: A 2D numpy array of ``np.double``, of size
-        :math:`N \cross N`.
-    :param extra_mass_penalty: The penalty for extra mass. If you want the
-        resulting distance to be a metric, it should be at least half the
-        diameter of the space (maximum possible distance between any two
-        points). If you want partial matching you can set it to zero (but then
-        the resulting distance is not guaranteed to be a metric). The default
-        value is -1 which means the maximum value in the distance matrix is
-        used.
+    Args:
+        first_signature (np.ndarray): A 1-dimensional array of type
+            ``np.double``, of length :math:`N`.
+        second_signature (np.ndarray): A 1-dimensional array of ``np.double``,
+            also of length :math:`N`..
+        distance_matrix: A 2-dimensional  array of ``np.double``, of size
+            :math:`N \cross N`.
+        extra_mass_penalty: The penalty for extra mass. If you want the
+            resulting distance to be a metric, it should be at least half the
+            diameter of the space (maximum possible distance between any two
+            points). If you want partial matching you can set it to zero (but then
+            the resulting distance is not guaranteed to be a metric). The default
+            value is -1, which means the maximum value in the distance matrix is
+            used.
+
+    Returns:
+        (emd, flows): The EMD value and the associated minimum-cost flows.
     """
     if (first_signature.shape[0] > distance_matrix.shape[0] or
             second_signature.shape[0] > distance_matrix.shape[0]):
@@ -74,22 +77,25 @@ def emd(np.ndarray[np.float64_t, ndim=1, mode="c"] first_signature,
         np.ndarray[np.float64_t, ndim=2, mode="c"] distance_matrix,
         extra_mass_penalty=-1.0):
     """
-    Compute the Earth Mover's Distance between the given signatures with the
-    given distance matrix.
+    Compute the EMD between signatures with the given distance matrix.
 
-    :param first_signature: A 1D numpy array of ``np.double``, of length
-        :math:`N`.
-    :param second_signature: A 1D numpy array of ``np.double``, also of length
-        :math:`N`..
-    :param distance_matrix: A 2D numpy array of ``np.double``, of size
-        :math:`N \cross N`.
-    :param extra_mass_penalty: The penalty for extra mass. If you want the
-        resulting distance to be a metric, it should be at least half the
-        diameter of the space (maximum possible distance between any two
-        points). If you want partial matching you can set it to zero (but then
-        the resulting distance is not guaranteed to be a metric). The default
-        value is -1 which means the maximum value in the distance matrix is
-        used.
+    Args:
+        first_signature (np.ndarray): A 1-dimensional array of type
+            ``np.double``, of length :math:`N`.
+        second_signature (np.ndarray): A 1-dimensional array of ``np.double``,
+            also of length :math:`N`..
+        distance_matrix: A 2-dimensional  array of ``np.double``, of size
+            :math:`N \cross N`.
+        extra_mass_penalty: The penalty for extra mass. If you want the
+            resulting distance to be a metric, it should be at least half the
+            diameter of the space (maximum possible distance between any two
+            points). If you want partial matching you can set it to zero (but then
+            the resulting distance is not guaranteed to be a metric). The default
+            value is -1, which means the maximum value in the distance matrix is
+            used.
+
+    Returns:
+        emd: The EMD value.
     """
     if (first_signature.shape[0] > distance_matrix.shape[0] or
             second_signature.shape[0] > distance_matrix.shape[0]):
