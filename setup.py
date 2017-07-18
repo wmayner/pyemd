3f32e7ea0f103b997803d0c020249b2f00af7bba
Will Mayner <wmayner@gmail.com>
2017-07-18 18:48:22 -0500

    Add Python 2 classifier to `setup.py`

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
