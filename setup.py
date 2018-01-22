a95667e5746ca59a7e8b6d8349ef8d6258d781ea
Will Mayner <wmayner@gmail.com>
2018-01-22 16:58:33 -0600

    setup.py: Fix classifiers

--- setup.py
+++ setup.py
@@ -114,10 +114,9 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
